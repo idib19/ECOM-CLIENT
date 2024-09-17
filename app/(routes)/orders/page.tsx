b6de075c2fa3fc7c7b4a80301ec76d12228880fd
Idrissa <idiberthe19@gmail.com>
2024-09-17 13:27:51 -0400

    Fixing the auth issues on the orders page

--- app/(routes)/orders/page.tsx
+++ app/(routes)/orders/page.tsx
@@ -1,9 +1,16 @@
 import OrdersPageClient from "@/components/orders/ordersPage"
 import { getUserOrders } from "@/actions/get-orders";
 import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 
 const OrdersPage = async () => { 
+
     const { userId } = auth();
+
+    if (!userId) {
+        // User is not authenticated, redirect to login page
+        redirect("/sign-in");
+    }
 
     const orders = await getUserOrders(userId);
 
